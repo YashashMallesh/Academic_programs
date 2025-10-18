609530638ba70d0b2c7d87dbb1f857cf0a5624dc
Yashash Mallesh <yashumallesh2022@gmail.com>
2025-10-18 18:14:32 +0530

    Update operationsonstack.c

--- dsa/operationsonstack.c
+++ dsa/operationsonstack.c
@@ -9,5 +9,15 @@ void palindrome();
 void display();
 void main()
 {
-  
+  int choice, item;
+  while(1)
+    {
+      printf("\n\n\n----Menu----");
+      printf("\n_1.Push an Element to Stack and Overflow demo");
+      printf("\n_2.Pop an element from stack and underflow demo");
+      printf("\n_3.Palindrome demo");
+      printf("\n_4.Display");
+      printf("\n_5.Exit");
+      printf("\nEnter your choice");
+    }
 }
