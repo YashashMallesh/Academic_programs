1e052c09a4abc445f2d37cd0c974f2d3fa3fa232
Yashash Mallesh <yashumallesh2022@gmail.com>
2025-10-22 19:00:00 +0530

    Update callow.c

--- dsa/callow.c
+++ dsa/callow.c
@@ -1,3 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main{}
+void main()
+{
+  int *ptr;
+  int i,n;
+  printf("Enter the no. of integers:\n");
+  scanf("%d",&n);
+  ptr = (int*)malloc(n*sizeof(int));
+  if(ptr==NULL)
+  {
+    printf("Memory not allocated");
+    exit(1);
+  }
+  for(i=0;i<n;i++)
+    {
+      printf("Enter The Integers\n");
+      scanf("%d",ptr+i);
+    }
+  for(i=0;i<n;i++)
+    {
+      printf("%d",*(ptr+i));
+    }
+  return(0);
+}
