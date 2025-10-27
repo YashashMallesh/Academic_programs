9adf3f7643bc5a1cc7c2519220df9644893a6a93
Yashash Mallesh <yashumallesh2022@gmail.com>
2025-10-27 19:07:53 +0530

    Update instance_var_hiding.java

--- java/instance_var_hiding.java
+++ java/instance_var_hiding.java
@@ -1,5 +1,16 @@
 class Example{
+  private int x = 10;
+  public void setX(int x){
+    System.out.println("Parameter x="+x);
+    this.x=x;
+  }
+  public void display(){
+    System.out.println("Instance variable x="+x);
+  }
+  
   public static void main(String[] args){
-    
+    Example obj = new Example();
+    obj.setX(20);
+    obj.display();
   }
 }
