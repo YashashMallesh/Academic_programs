5d47b439e2c12a34bb1f345d2ec855f6ba6f1309
Yashash Mallesh <yashumallesh2022@gmail.com>
2025-12-18 19:40:22 +0530

    Create BinarysearchTree.c

---
+++ dsa/BinarysearchTree.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+struct node{
+int info;
+struct node *lchild,*rchild;
+};
+typedef struct node *NODE;
+int main(){
+}
+NODE CREATENODE(NODE root,int data){
+}
+void INORDER(NODE root){
+}
+void PREORDER(NODE root){
+}
+void POSTORDER(NODE root){
+}
+NODE SERACH_NODE(NODE root,int key){
+}
