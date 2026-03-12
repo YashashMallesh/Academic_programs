d1c94bfb9193c1c12db7c71ed9bbc278b70f1ad3
Yashash Mallesh <yashumallesh2022@gmail.com>
2026-03-12 19:24:26 +0530

    Update quicksort.c

--- ada/quicksort.c
+++ ada/quicksort.c
@@ -6,3 +6,43 @@ void swap(long int *a,long int *b){
   *a = *b;
   *b = tmp;
 }
+long int partition(long int arr[],long int low,long int high){
+  long int pivot = arr[high];
+  long int i = low-1;
+  for(long int j=low;j<=high;j++){
+    if(arr[j]<=pivot){
+      i++;
+      swap(&arr[i],&arr[j]);
+    }
+  }
+  swap(&arr[i+1],&arr[high]);
+  return(i+1);
+}
+void quicksort(long int arr[],long int low,long int high){
+  if(low<high){
+    long int pi = partition(arr,low,high);
+    quicksort(arr,low,pi-1);
+    quicksort(arr,pi+1,high);
+  }
+}
+int main(){
+  long int n = 10000;
+  int it = 0;
+  double time[10];
+  printf("A_Size,Quick\n");
+  while(it++<10){
+    long int a[n];
+    for(int i=0;i<n;i++){
+      long int num = rand()%n+1;
+      a[i] = num;
+    }
+    clock_t start,end;
+    start = clock();
+    quicksort(a,0,n-1);
+    end = clock();
+    time[it] = ((double)(end-start)/CLOCKS_PER_SEC);
+    printf("%li,%f\n",n,time[it]);
+    n<=10000;
+  }
+  return 0;
+}
