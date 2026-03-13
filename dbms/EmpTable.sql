b764e5bd0dff5a11b8d50a5e791d718a91959a59
Yashash Mallesh <yashumallesh2022@gmail.com>
2026-03-13 19:18:17 +0530

    Update EmpTable.sql

--- dbms/EmpTable.sql
+++ dbms/EmpTable.sql
@@ -6,9 +6,9 @@ grant create view to c##filename;
 connect c##filename/password;
 
 create table Emp(Empno number(3), Ename varchar(15), Job varchar(10), Mno(3), sal number(10,2));
-insert into Emp values(1,'Job','Manager',NULL,5000,1000);
-insert into Emp valueS(2,'Smith','Dev',1,4000,NULL);
-insert into Emp values(3,'Richard','CEO',NULL,5000,1000);
+insert into Emp values(1,'Job','Manager',NULL,5000);
+insert into Emp valueS(2,'Smith','Dev',1,4000);
+insert into Emp values(3,'Richard','CEO',NULL,5000);
 select * from Emp;
 desc Emp;
 delete * from Emp;
@@ -18,4 +18,18 @@ commit;
 delete * from Emp;
 select * from Emp;
 rollback;
+select * from Emp;
+
+DROP table Emp;
+
+create table Emp(Empno number(3), Ename varchar(15), Job varchar(10), Mno(3), sal number(10,2));
+alter table Emp add com decimal(10,2);
+desc Emp;
+insert into Emp values(1,'Job','Manager',2,5000,1000);
+insert into Emp valueS(2,'Smith','Dev',1,4000,2000);
+insert into Emp values(3,'Richard','CEO',3,5000,1000);
+alter table Emp rename column mgr to mgrid;
+desc Emp;
+UPDATE Emp set job = 'ProjectMgr' where mgrid = 1;
+delete from Emp where Empno = 105;
 select * from Emp;
