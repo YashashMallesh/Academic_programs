10f6c541741af560b1147b2958fe819f24fb742b
Yashash Mallesh <yashumallesh2022@gmail.com>
2026-03-22 15:24:40 +0530

    Create arlist2.java

---
+++ java/Collections/arlist2.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Random;
+
+public class Program2{
+  public static void main(String args[]){
+    
+  }
+}
