2210ddd5acc28de30854cde3dd9d2fafc80ba2f3
Yashash Mallesh <yashumallesh2022@gmail.com>
2026-03-26 20:09:47 +0530

    Create Mergesort.c

---
+++ ada/Mergesort.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void merge(long int arr[],long int low,long int mid,long int high){
+  
+}
+void mergesort(long int arr[],long int low,long int high){
+  
+}
+int main(){
+  
+}
