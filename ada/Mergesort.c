4128b3e384e5f6b027f67d51b6eb2a1304087bff
Yashash Mallesh <yashumallesh2022@gmail.com>
2026-03-27 20:59:43 +0530

    Add merge function to Mergesort implementation
    Implemented the merge function for Mergesort algorithm.

--- ada/Mergesort.c
+++ ada/Mergesort.c
@@ -3,8 +3,30 @@
 #include<time.h>
 
 void merge(long int arr[],long int low,long int mid,long int high){
-  
-}
+  long int k = low;
+  long int i = low;
+  long int j = mid+1;
+  long int temp[100000];
+  while(i<=mid && j<=high){
+    if(arr[i]<=arr[j]){
+      temp[k] = arr[i];
+      i++;
+      k++;
+    }
+    else{
+      temp[k] = arr[j];
+      j++;
+      k++;
+    }
+  }
+  while(i<=mid){
+    temp[k]  = arr[i];
+    i++;
+    k++;
+  }
+  for(i=low;i<=high;i++)
+    arr[i] = temp[i];
+  }
 void mergesort(long int arr[],long int low,long int high){
   
 }
