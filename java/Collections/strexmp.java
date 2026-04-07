0a0b4c5d40de60196741a1343df5e05126da3333
Yashash Mallesh <yashumallesh2022@gmail.com>
2026-04-07 21:27:25 +0530

    Update strexmp.java

--- java/Collections/strexmp.java
+++ java/Collections/strexmp.java
@@ -1,7 +1,11 @@
-import java.UnsupportedEncodingException;
+import java.io.UnsupportedEncodingException;
 
 public class strexmp{
   public static void main(String args[]){
+    String str1 = new String();
+    System.out.println("Default Constructor"+str1);
     
+    String str2 = new String("Hello World!");
+    System.out.println("Constructor with string argument"+str2);
   }
 }
