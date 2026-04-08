a320829e001ec359dcc1a40b3b3bcb344214c5c5
Yashash Mallesh <yashumallesh2022@gmail.com>
2026-04-08 19:55:28 +0530

    Update strexmp.java

--- java/Collections/strexmp.java
+++ java/Collections/strexmp.java
@@ -7,5 +7,14 @@ public class strexmp{
     
     String str2 = new String("Hello World!");
     System.out.println("Constructor with string argument"+str2);
+
+    char[] chararray = {'H','e','l','l','o'};
+    String str3  = new String(chararray);
+    System.out.println("Constructor with Character Array"+str3);
+
+    char[] chararrayoffset = {'H','e','l','l','o',',','W','o','r','l','d'};
+    String str4  = new String(chararrayoffset);
+    System.out.println("Constructor with Character Array and Offset"+str4);
+
   }
 }
