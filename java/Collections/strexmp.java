cbee545b1224693c1ec2585cd7015883027ba6e9
Yashash Mallesh <yashumallesh2022@gmail.com>
2026-04-08 20:01:59 +0530

    Enhance strexmp.java with String constructor examples
    Added examples of String constructors using byte arrays with various parameters.

--- java/Collections/strexmp.java
+++ java/Collections/strexmp.java
@@ -16,5 +16,23 @@ public class strexmp{
     String str4  = new String(chararrayoffset);
     System.out.println("Constructor with Character Array and Offset"+str4);
 
+    byte[] bytearray = {72,101,108,108,111};
+    String str5  = new String(bytearray);
+    System.out.println("Constructor with Byte Array"+str5);
+    try{
+    byte[] bytearraycharset = {72,101,108,108,111};
+    String str6  = new String(bytearraycharset,"UTF-8");
+    System.out.println("Constructor with Byte Array and charset"+str6);
+      
+    byte[] bytearrayoffset = {72,101,108,108,111,32,87,111,114,108,100};
+    String str7  = new String(bytearrayoffset,6,8);
+    System.out.println("Constructor with Byte Array and Offset"+str7);
+
+    byte[] bytearrayoffsetcharset = {72,101,108,108,111,32,87,111,114,108,100};
+    String str8  = new String(bytearrayoffsetcharset,6,8,"UTF-8");
+    System.out.println("Constructor with Byte Array, Offset, Length and Charset"+str8);
+    }catch(UnsupportedEncodingException e){
+      System.out.println("Error:Unsupprted Encoding Exception.");
+    }
   }
 }
