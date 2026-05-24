3c87b469e6813e603bb1a387b7744486be404337
Yashash <yashumallesh2022@gmail.com>
2026-05-24 18:12:45 +0530

    Update SelectionSort.c

--- ada/SelectionSort.c
+++ ada/SelectionSort.c
@@ -51,12 +51,13 @@ The boundary moves one element to the right, and the process repeats until the e
 The main function runs 10 iterations*/
 /*DrawbacksPoor Scaling {O}(n^2): It becomes incredibly slow for large datasets.
 Doubling the array size increases the execution time by roughly a factor of 4.
+
 Not Adaptive: It does not care if the data is already perfectly sorted. It will stupidly run through every loop iteration anyway.
 Unstable Sorting: It can change the relative order of elements with equal values. For example, if you sort [4a, 4b, 1], 4a will be swapped with 1, resulting in [1, 4b, 4a]. 
 The original order of the fours is broken.
 
 Hardware with Costly Write Operations: In flash memory (like EEPROM), writing data degrades the hardware much faster than reading it. 
-Selection Sort performs at most $\mathcal{O}(n)$ swaps (writes), making it highly optimal when write-cycles are precious.
+Selection Sort performs at most {O}(n) swaps (writes), making it highly optimal when write-cycles are precious.
 Strictly Auxiliary Memory Constrained Systems: Because it is an in-place algorithm requiring zero extra memory arrays, it works well in deeply embedded systems with just bytes of RAM.
-Small Datasets: For tiny lists (e.g., $n < 20$), the overhead of complex algorithms like Quick Sort or Merge Sort makes Selection Sort perfectly acceptable and easy to debug.
+Small Datasets: For tiny lists (e.g., n < 20), the overhead of complex algorithms like Quick Sort or Merge Sort makes Selection Sort perfectly acceptable and easy to debug.
 */
