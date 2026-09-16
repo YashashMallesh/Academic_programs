330ee9dde840e0d00950f011adb52cd98b2eb0b4
Yashash <yashumallesh2022@gmail.com>
2026-09-16 19:05:50 +0530

    Update p1.tcl

--- networks/p1/p1.tcl
+++ networks/p1/p1.tcl
@@ -1,21 +1,21 @@
 set ns [new Simulator] 
-set nf [open lab1.nam w] 
+set nf [open p1.nam w] 
 $ns namtrace-all $nf 
-set tf [open lab1.tr w] 
-$ns trace-all $tf
+set nd [open p1.tr w] 
+$ns trace-all $nf
 proc finish { } {
-global ns nf tf
+global ns nf nd
 $ns flush-trace 
 close $nf
-close $tf
-exec nam lab1.nam 
+close $nf
+exec nam p1.nam &
 exit 0
 }
 set n0 [$ns node] 
 set n2 [$ns node]
 set n3 [$ns node]
-$ns duplex-link $n0 $n2 200Mb 10ms DropTail 
-$ns duplex-link $n2 $n3 1Mb 1000ms DropTail
+$ns duplex-link $n0 $n2 1Mb 10ms DropTail 
+$ns duplex-link $n2 $n3 512kb 10ms DropTail
 $ns queue-limit $n0 $n2 10
 set udp0 [new Agent/UDP] 
 $ns attach-agent $n0 $udp0
@@ -23,9 +23,10 @@ set cbr0 [new Application/Traffic/CBR]
 $cbr0 set packetSize_ 500
 $cbr0 set interval_ 0.005
 $cbr0 attach-agent $udp0
-set null0 [new Agent/Null] 
-$ns attach-agent $n3 $null0
-$ns connect $udp0 $null0
-$ns at 0.1 "$cbr0 start"
-$ns at 1.0 "finish"
+set sink [new Agent] 
+$ns attach-agent $n3 $sink
+$ns connect $udp0 $sink
+$ns at 0.2 "$cbr0 start"
+$ns at 4.5 "$cbr0 stop"
+$ns at 5.0 "finish"
 $ns run
